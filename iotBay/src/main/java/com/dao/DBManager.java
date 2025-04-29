c384a080c1866927bef5d4c94d46662c21df0ea2
sazzh <100480867+sazzh@users.noreply.github.com>
2025-04-30 02:08:03 +1000

    Add connection to CustomerDao.java

--- iotBay/src/main/java/com/dao/DBManager.java
+++ iotBay/src/main/java/com/dao/DBManager.java
@@ -5,11 +5,19 @@ import java.sql.*;
 public class DBManager {
     private final Connection connection;
 
+    private CustomerDao customerDao;
     private ProductDao productDao;
     private StaffDao staffDao;
 
     public DBManager(Connection connection) throws SQLException {
         this.connection = connection;
+    }
+
+    public CustomerDao getCustomerDao() {
+        if (customerDao == null) {
+            customerDao = new CustomerDao(connection);
+        }
+        return customerDao;
     }
 
     public ProductDao getProductDao() {
