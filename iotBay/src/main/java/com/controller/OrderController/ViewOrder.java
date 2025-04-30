53ac58b79f3b08d1bbcab52b7c5bd575eaeb2017
zizhu10y <sarahzhao10y@gmail.com>
2025-04-30 17:17:34 +1000

    1

--- iotBay/src/main/java/com/controller/OrderController/ViewOrder.java
+++ iotBay/src/main/java/com/controller/OrderController/ViewOrder.java
@@ -1,4 +1,34 @@
 package com.controller.OrderController;
 
-public class ViewOrder {
+import com.bean.Order;
+import com.dao.OrderDao;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/viewOrder")
+public class ViewOrder extends HttpServlet {
+    private OrderDao orderDao;
+
+    @Override
+    public void init() {
+        Connection connection = (Connection) getServletContext().getAttribute("dbConn");
+        orderDao = new OrderDao(connection);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        String orderIdPara = request.getParameter("orderId");
+        String orderDatePara = request.getParameter("orderDate");
+
+        List<Order> orderList = new ArrayList<>();
+        int customerId = 1; //模拟ID 后续从session获取
+    }
 }
--- iotBay/src/main/java/com/dao/OrderDao.java
+++ iotBay/src/main/java/com/dao/OrderDao.java
@@ -1,4 +1,8 @@
 package com.dao;
 
+import java.sql.Connection;
+
 public class OrderDao {
+    public OrderDao(Connection connection) {
+    }
 }
