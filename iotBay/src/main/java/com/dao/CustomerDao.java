003c0d06c4387f921c73edeaa5534c12e68c06f2
sazzh <100480867+sazzh@users.noreply.github.com>
2025-05-04 02:29:44 +1000

    Add phone field for users

--- iotBay/src/main/java/com/dao/CustomerDao.java
+++ iotBay/src/main/java/com/dao/CustomerDao.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -20,6 +21,7 @@ public class CustomerDao {
     public void addUser(Customer customer) throws SQLException {
         PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO User (username, first_name, last_name, " +
                 "password, email, status, address, state, city, postcode, country) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+                "password, email, phone, status, address, state, city, postcode, country) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
         preparedStatement.setString(1, customer.getUsername());
         preparedStatement.setString(2, customer.getFirstName());
         preparedStatement.setString(3, customer.getLastName());
@@ -33,6 +35,7 @@ public class CustomerDao {
         preparedStatement.setString(11, customer.getCountry());
         // add phone number to db
 //        preparedStatement.setLong(12, customer.getPhone());
+        preparedStatement.setLong(6, customer.getPhone());
         // add history to db
 //        preparedStatement.setString(13, customer.getHistory().toString());
         preparedStatement.execute();
