87aa6f22a3b2621566907a000ed15dcefda60590
StarStriker <77044214+luckyseo@users.noreply.github.com>
2025-05-06 09:49:16 +1000

    forgot to add new servlet for search

---
+++ iotBay/src/main/java/com/controller/ProductController/GetByProductNameToCustomer.java
@@ -0,0 +1,41 @@
+package com.controller.ProductController;
+
+import com.bean.Product;
+import com.dao.DBManager;
+import com.dao.ProductDao;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet("/GetByProductNameToCustomer")
+public class GetByProductNameToCustomer extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try{
+            HttpSession session = req.getSession();
+            DBManager db = (DBManager) session.getAttribute("db");
+            ProductDao productDao = db.getProductDao();
+
+            String productName = req.getParameter("productName");
+            Product product = productDao.getProductByName(productName);
+            if (product != null) {
+                req.setAttribute("product", product);
+                req.getRequestDispatcher("/views/Search.jsp").forward(req, resp);
+            } else{
+                req.setAttribute("message", "404 NotFound");
+                req.getRequestDispatcher("/views/Search.jsp").forward(req,resp);
+            }
+
+        } catch(SQLException | IOException e){
+            System.out.println("Failed to load a product");
+            throw new ServletException(e);
+        }
+
+
+    }
+}
