098d1fff1877fd70578e2a372b0ff91df288e0ad
sazzh <100480867+sazzh@users.noreply.github.com>
2025-05-07 02:16:33 +1000

    Add phone formatting method for display

--- iotBay/src/main/java/com/util/Utils.java
+++ iotBay/src/main/java/com/util/Utils.java
@@ -8,4 +8,17 @@ public final class Utils {
         return lowercaseInput.substring(0, 1).toUpperCase() + lowercaseInput.substring(1);
     }
 
+    public static String formatPhoneNumber(Long phoneNo) {
+        if (phoneNo == null) return "";
+        String phoneAsString  = String.valueOf(phoneNo);
+
+        // Add 0 at start of phone number
+        if (phoneAsString.length() == 9) {
+            phoneAsString = "0" +  phoneAsString;
+        }
+
+        if (phoneAsString.length() != 10) return phoneAsString;
+        return phoneAsString.replaceFirst("(\\d{4})(\\d{3})(\\d{3})", "$1 $2 $3");
+    }
+
 }
