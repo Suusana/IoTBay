f1aa725275086b048124bfd7b539a9c104320398
sazzh <100480867+sazzh@users.noreply.github.com>
2025-05-08 03:29:22 +1000

    Create EditUserDetailsServlet.java

---
+++ iotBay/src/main/java/com/controller/UserAccessController/EditUserDetailsServlet.java
@@ -0,0 +1,46 @@
+package com.controller.UserAccessController;
+
+import com.bean.Customer;
+import com.dao.CustomerDao;
+import com.dao.DBManager;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet("/EditUserDetailsServlet")
+public class EditUserDetailsServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        DBManager db =  (DBManager) session.getAttribute("db");
+        CustomerDao customerDao = db.getCustomerDao();
+        Customer customer = (Customer) session.getAttribute("loggedInUser");
+
+        if (customer == null) {
+            session.setAttribute("errorMessage", "Please login to edit your profile");
+            resp.sendRedirect(req.getContextPath()+"/views/login.jsp");
+            return;
+        }
+
+        Customer updatedCustomer;
+        try {
+            // update customer to latest data from database before displaying
+            updatedCustomer = customerDao.getUserById(customer.getUserId());
+            session.setAttribute("loggedInUser",  updatedCustomer);
+            resp.sendRedirect(req.getContextPath()+"/views/editUserDetails.jsp");
+        } catch (SQLException e) {
+            System.out.println("Couldn't retrieve user");
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+}
