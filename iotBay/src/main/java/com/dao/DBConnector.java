7c7091895db3294a86bc2ee3bcb6d718df2f0448
Suin_ <Suin.Lee-2@student.uts.edu.au>
2025-05-08 22:22:21 +1000

    Update DBConnector.java

--- iotBay/src/main/java/com/dao/DBConnector.java
+++ iotBay/src/main/java/com/dao/DBConnector.java
@@ -1,10 +1,8 @@
 package com.dao;
 
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 
 public class DBConnector {
     private Connection connection;
@@ -13,16 +11,17 @@ public class DBConnector {
         System.setProperty("org.sqlite.lib.verbose", "true");
         try {
             Class.forName("org.sqlite.JDBC");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        String url = "jdbc:sqlite:IotBayDB.db";
-        try {
+
+            String url = "jdbc:sqlite:/Users/suinlee/Desktop/apache-tomcat-10.1.39/webapps/ROOT/IoTBayDB.db";
             connection = DriverManager.getConnection(url);
             connection.setAutoCommit(true);
-            System.out.println("Connected to database");
-        }
-        catch (SQLException e) {
+            System.out.println("✅ Connected to database");
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("XX. SQLite JDBC Driver not found");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("XX. Failed to connect to SQLite DB");
             e.printStackTrace();
         }
     }
@@ -37,8 +36,7 @@ public class DBConnector {
                 connection.close();
                 System.out.println("Connection closed");
             }
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
