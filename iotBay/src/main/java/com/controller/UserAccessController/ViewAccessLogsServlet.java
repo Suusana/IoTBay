be94bf77e4e8de0b953207ce875c324beb29d963
sazzh <100480867+sazzh@users.noreply.github.com>
2025-05-12 01:06:35 +1000

    Allow staff to view their access logs

--- iotBay/src/main/java/com/controller/UserAccessController/ViewAccessLogsServlet.java
+++ iotBay/src/main/java/com/controller/UserAccessController/ViewAccessLogsServlet.java
@@ -1,9 +1,11 @@
 package com.controller.UserAccessController;
 
 import com.bean.Customer;
+import com.bean.Staff;
 import com.bean.UserAccessLog;
 import com.dao.CustomerDao;
 import com.dao.DBManager;
+import com.dao.StaffDao;
 import com.dao.UserAccessLogDao;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -23,17 +25,44 @@ public class ViewAccessLogsServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
         DBManager db =  (DBManager) session.getAttribute("db");
-        Customer customer = (Customer) session.getAttribute("loggedInUser");
-        String userType = session.getAttribute("userType").toString().toLowerCase();
+        String userType = (String) session.getAttribute("userType");
         UserAccessLogDao userAccessLogDao = db.getUserAccessLogDao();
 
-        try {
-            LinkedList<UserAccessLog> accessLogs = userAccessLogDao.getLogsByUser(customer.getUserId(), userType);
-            req.setAttribute("accessLogs", accessLogs);
-            req.getRequestDispatcher("/views/viewAccessLogs.jsp").forward(req, resp);
-        } catch (SQLException e) {
-            System.out.println("Could not retrieve user's access logs");
-            throw new RuntimeException(e);
+        if (userType == null) {
+            session.setAttribute("errorMessage", "Please login before accessing account history");
+            resp.sendRedirect(req.getContextPath()+"/views/login.jsp");
+            return;
         }
+
+        Customer customer;
+        Staff staff;
+        // get access logs if customer
+        if (userType.equalsIgnoreCase("Customer")) {
+            customer = (Customer) session.getAttribute("loggedInUser");
+
+            try {
+                LinkedList<UserAccessLog> accessLogs = userAccessLogDao.getLogsByUser(customer.getUserId(), userType);
+                req.setAttribute("accessLogs", accessLogs);
+                req.getRequestDispatcher("/views/viewAccessLogs.jsp").forward(req, resp);
+            } catch (SQLException e) {
+                System.out.println("Could not retrieve customer's access logs");
+                throw new RuntimeException(e);
+            }
+            // get access logs if staff
+        } else if (userType.equalsIgnoreCase("Staff")) {
+            staff = (Staff) session.getAttribute("loggedInUser");
+
+            LinkedList<UserAccessLog> accessLogs = null;
+            try {
+                accessLogs = userAccessLogDao.getLogsByUser(staff.getStaffId(), userType);
+                req.setAttribute("accessLogs", accessLogs);
+                req.getRequestDispatcher("/views/viewAccessLogs.jsp").forward(req, resp);
+            } catch (SQLException e) {
+                System.out.println("Could not retrieve staff's access logs");
+                throw new RuntimeException(e);
+            }
+
+        }
+
     }
 }
